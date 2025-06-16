ca28af520bcdfbf4e49303e57da059900d25c23a
puru-verma <purshottamvermakv5@gmail.com>
2025-06-16 08:50:19 +0530

    new folder structure updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medibot-Chat-Assistant',
+    version='0.0.0',
+    author='Puru',
+    author_email='puruverma@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
